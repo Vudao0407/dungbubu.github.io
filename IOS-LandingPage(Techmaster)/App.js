688ba58cc75aa1a7942b7d113f4dc1f71474913b
dungbubu <nguyenmanhdung1996@gmail.com>
2021-04-01 23:18:31 +0700

    1/4/21

---
+++ IOS-LandingPage(Techmaster)/App.js
@@ -0,0 +1,299 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import ModalAdd from "./component/ModalAdd";
+import ModalDelete from "./component/ModalDelete";
+import ModalEdit from "./component/ModalEdit";
+import { Button, Modal } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
+
+const STUDENTLIST = [
+  {
+    id: 1,
+    name: "Hieu",
+    dob: "20/02/1994",
+    email: "ABC@gmail.com",
+    phone: "01242433434",
+  },
+  {
+    id: 2,
+    name: "Dat",
+    dob: "20/02/1994",
+    email: "ABC@gmail.com",
+    phone: "01242433434",
+  },
+  {
+    id: 3,
+    name: "Dung",
+    dob: "20/02/1994",
+    email: "ABC@gmail.com",
+    phone: "01242433434",
+  },
+  {
+    id: 4,
+    name: "Huy",
+    dob: "20/02/1994",
+    email: "ABC@gmail.com",
+    phone: "01242433434",
+  },
+];
+
+function App() {
+  // const [users, setUsers] = useState();
+  const [students, setStudentList] = useState(STUDENTLIST);
+  useEffect(() =>{
+    console.log("component did mounted");
+    async function getUsers(){
+      const res = await fetch("http://localhost:3000/users");
+      const data = await res.json();
+      setStudentList(data);
+    }
+    getUsers();
+
+  }, []);
+
+
+  
+  const [modalAdd, modalAddVisible] = useState(false);
+  const [modalDelete, modalDeleteVisible] = useState(false);
+  const [buttonDeleteId, setButtonDeleteId] = useState();
+
+  // Tạo mới
+  const [inputName, setInputName] = useState();
+  const [inputDob, setInputDob] = useState();
+  const [inputEmail, setInputEmail] = useState();
+  const [inputPhone, setInputPhone] = useState();
+
+  function updateName(event) {
+    const inputNameValue = event.target.value;
+    setInputName(inputNameValue);
+  }
+
+  function updateDob(event) {
+    const inputDobValue = event.target.value;
+    setInputDob(inputDobValue);
+  }
+  function updateEmail(event) {
+    const inputEmailValue = event.target.value;
+    setInputEmail(inputEmailValue);
+  }
+  function updatePhone(event) {
+    const inputPhoneValue = event.target.value;
+    setInputPhone(inputPhoneValue);
+  }
+
+  // Chỉnh sửa
+  // Dùng map hoặc vòng lặp for
+
+  const [inputNameEdit, setInputNameEdit] = useState();
+  const [inputDobEdit, setInputDobEdit] = useState();
+  const [inputEmailEdit, setInputEmailEdit] = useState();
+  const [inputPhoneEdit, setInputPhoneEdit] = useState();
+
+  function updateNameEdit(event) {
+    const inputNameValueEdit = event.target.value;
+    setInputNameEdit(inputNameValueEdit);
+  }
+  function updateDobEdit(event) {
+    const inputDobValueEdit = event.target.value;
+    setInputDobEdit(inputDobValueEdit);
+  }
+  function updateEmailEdit(event) {
+    const inputEmailValueEdit = event.target.value;
+    setInputEmailEdit(inputEmailValueEdit);
+  }
+  function updatePhoneEdit(event) {
+    const inputPhoneValueEdit = event.target.value;
+    setInputPhoneEdit(inputPhoneValueEdit);
+  }
+
+  const studentlist = students.map((student) => (
+    <tr key={student.id}>
+      <td>{student.name}</td>
+      <td>{student.dob}</td>
+      <td>{student.email}</td>
+      <td>{student.phone}</td>
+      <td>
+        <button className="edit" onClick={() => handleShow(student.id)}>
+          Chỉnh sửa
+        </button>
+        <button className="delete" onClick={() => openModalDelete(student.id)}>
+          {" "}
+          Xóa
+        </button>
+      </td>
+    </tr>
+  ));
+
+  // Mở modal thêm sinh viên
+  function openModalAdd() {
+    modalAddVisible(true);
+  }
+
+  // Tắt modal thêm sinh viên
+  function backToList() {
+    setStudentList(students);
+    modalAddVisible(false);
+  }
+
+  const [newStudentId, setNewStudentId] = useState(4);
+
+  // Thêm sinh viên
+  function saveNewStudent() {
+    let newId = newStudentId + 1;
+    setNewStudentId(newId);
+
+    let newStudent = {
+      id: newId,
+      name: inputName,
+      dob: inputDob,
+      email: inputEmail,
+      phone: inputPhone,
+    };
+
+    students.unshift(newStudent);
+    console.log(students);
+    modalAddVisible(false);
+  }
+
+  // Mở modal xóa sinh viên
+  function openModalDelete(studentId) {
+    setButtonDeleteId(studentId);
+    modalDeleteVisible(true);
+  }
+
+  // Confirm xóa sinh viên
+  function confirmRemove() {
+    const newStudentList = students.filter(
+      (student) => student.id !== buttonDeleteId
+    );
+
+    setStudentList(newStudentList);
+    modalDeleteVisible(false);
+  }
+
+  // Không xóa sinh viên
+  function cancelRemove() {
+    modalDeleteVisible(false);
+  }
+
+  // Chỉnh sửa thông tin sinh viên
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+
+  const [editStudentId, setEditStudentId] = useState();
+
+  function handleShow(studentID) {
+
+    let index = students.findIndex((student) => student.id === studentID);
+    if (index > -1) {
+      let newStudent = [...students];
+      setEditStudentId(studentID);
+      setInputNameEdit(newStudent[index].name);
+      setInputDobEdit(newStudent[index].dob);
+      setInputEmailEdit(newStudent[index].email);
+      setInputPhoneEdit(newStudent[index].phone);
+    }
+
+    setShow(true);
+  }
+  function handleEdit() {
+    let index = students.findIndex((student) => student.id === editStudentId);
+    if (index > -1) {
+      let newStudentList = [...students];
+      newStudentList[index].name = inputNameEdit;
+      newStudentList[index].dob = inputDobEdit;
+      newStudentList[index].email = inputEmailEdit;
+      newStudentList[index].phone = inputPhoneEdit;
+
+      setStudentList(newStudentList);
+    }
+
+    console.log(students);
+    setShow(false);
+  }
+
+  // Sắp xếp dữ liệu
+
+  function compare(a, b) {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  }
+
+  function sortName() {
+    students.sort(compare);
+  }
+
+  return (
+    <main>
+      <div className="container">
+        <h1 className="title">Danh sách học viên</h1>
+        <button id="add-student" onClick={openModalAdd}>
+          Thêm học viên
+        </button>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>
+                  Họ tên{" "}
+                  <button className="fa fa-sort" onClick={sortName}></button>
+                </th>
+                <th>Năm sinh</th>
+                <th>Email</th>
+                <th>Số điện thoại</th>
+                <th></th>
+              </tr>
+            </thead>
+
+            <tbody id="content">{studentlist}</tbody>
+          </table>
+        </div>
+        {modalAdd && (
+          <ModalAdd
+            updateName={updateName}
+            updateDob={updateDob}
+            updateEmail={updateEmail}
+            updatePhone={updatePhone}
+            backToList={backToList}
+            saveNewStudent={saveNewStudent}
+          />
+        )}
+        {modalDelete && (
+          <ModalDelete
+            message="Bạn có muốn xóa sinh viên này không?"
+            confirmRemove={confirmRemove}
+            cancelRemove={cancelRemove}
+          />
+        )}
+
+        {show && (
+          <ModalEdit
+            Button={Button}
+            Modal={Modal}
+            show={show}
+            handleClose={handleClose}
+            students={students}
+            updateNameEdit={updateNameEdit}
+            updateDobEdit={updateDobEdit}
+            updateEmailEdit={updateEmailEdit}
+            updatePhoneEdit={updatePhoneEdit}
+            handleEdit={handleEdit}
+            inputNameEdit={inputNameEdit}
+            inputDobEdit={inputDobEdit}
+            inputEmailEdit={inputEmailEdit}
+            inputPhoneEdit={inputPhoneEdit}
+          />
+        )}
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ IOS-LandingPage(Techmaster)/js/index.js
@@ -0,0 +1,100 @@
+// ------------ ------------ ------------ ------------ ------------ ------------
+// Thiết lập Slick Carousel
+$('.carouselGiangVien').slick(
+  {
+    infinite: true,
+    autoplay: true,
+    speed: 1000,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    focusOnSelect: false,
+    variableWidth: true
+  });
+
+$('.carouselFeedback').slick(
+  {
+    infinite: true,
+    autoplay: true,
+    speed: 1000,
+    centerMode: true,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    focusOnSelect: false,
+    variableWidth: true
+  });
+
+
+// ------------ ------------ ------------ ------------ ------------ ------------
+// Hàm add/remove trạng thái Active khi "click" vào các nút trên Menu
+let menuItemActive = [...document.getElementsByClassName("itemMenu")];
+menuItemActive[0].classList.add("active");
+
+menuItemActive.forEach(function(element) 
+  {
+    element.addEventListener("click",function()
+      {
+        element.classList.add("active");
+        
+        var sibling = menuItemActive[0];
+        while (sibling) 
+        {
+            if (sibling.nodeType === 1 && sibling !== element) 
+              {
+                sibling.classList.remove("active");
+              }
+            sibling = sibling.nextSibling
+        }
+      });
+  });
+
+
+// ------------ ------------ ------------ ------------ ------------ ------------
+// Hàm add/remove trạng thái Active khi "scroll" qua các vị trí trên trang
+let idOffSet = [document.getElementById("idTrangChu").getBoundingClientRect().top + document.documentElement.scrollTop,
+                document.getElementById("idGioiThieu").getBoundingClientRect().top + document.documentElement.scrollTop,
+                document.getElementById("idGiangVien").getBoundingClientRect().top + document.documentElement.scrollTop,
+                document.getElementById("idNoiDung").getBoundingClientRect().top + document.documentElement.scrollTop,
+                document.getElementById("idHocVien").getBoundingClientRect().top + document.documentElement.scrollTop,
+                document.getElementById("idLienHe").getBoundingClientRect().top + document.documentElement.scrollTop,
+                document.getElementById("idBaiViet").getBoundingClientRect().top + document.documentElement.scrollTop];
+
+window.addEventListener("scroll",function()
+  {
+    let windowViewpoint = window.pageYOffset + 100;
+
+    menuItemActive.forEach(function(element)
+      {
+        element.classList.remove("active");
+      });
+
+    if (idOffSet[1] > windowViewpoint && windowViewpoint >= idOffSet[0])
+      {
+        menuItemActive[0].classList.add("active");
+      }
+    else if (idOffSet[2] > windowViewpoint && windowViewpoint >= idOffSet[1])
+      {
+        menuItemActive[1].classList.add("active");
+      }
+    else if (idOffSet[3] > windowViewpoint && windowViewpoint >= idOffSet[2])
+      {
+        menuItemActive[2].classList.add("active");
+      }
+    else if (idOffSet[4] > windowViewpoint && windowViewpoint >= idOffSet[3])
+      {
+        menuItemActive[3].classList.add("active");
+      }
+    else if (idOffSet[5] > windowViewpoint && windowViewpoint >= idOffSet[4])
+      {
+        menuItemActive[4].classList.add("active");
+      }
+    else if (idOffSet[6] > windowViewpoint && windowViewpoint >= idOffSet[5])
+      {
+        menuItemActive[5].classList.add("active");
+      }
+    else
+      {
+        menuItemActive[6].classList.add("active");
+      }
+  });
