a7e0de665f25b5c487f0c7832d3018ef94234e0b
dungbubu <nguyenmanhdung1996@gmail.com>
2021-04-03 23:37:01 +0700

    3/4/21

--- IOS-LandingPage(Techmaster)/js/index.js
+++ IOS-LandingPage(Techmaster)/js/index.js
@@ -29,7 +29,10 @@ $('.carouselFeedback, .carouselBlog').slick(
 // ------------ ------------ ------------ ------------ ------------ ------------
 // Hàm add/remove trạng thái Active khi "click" vào các nút trên Menu
 let menuItemActive = [...document.getElementsByClassName("itemMenu")];
+let burgerFilling = [...document.getElementsByClassName("burger-filling")];
+
 menuItemActive[0].classList.add("active");
+burgerFilling[0].classList.add("active");
 
 menuItemActive.forEach(function(element) 
   {
@@ -52,19 +55,23 @@ menuItemActive.forEach(function(element)
 
 // ------------ ------------ ------------ ------------ ------------ ------------
 // Hàm add/remove trạng thái Active khi "scroll" qua các vị trí trên trang
-let idOffSet = [document.getElementById("idTrangChu").getBoundingClientRect().top + document.documentElement.scrollTop,
-                document.getElementById("idGioiThieu").getBoundingClientRect().top + document.documentElement.scrollTop,
-                document.getElementById("idGiangVien").getBoundingClientRect().top + document.documentElement.scrollTop,
-                document.getElementById("idNoiDung").getBoundingClientRect().top + document.documentElement.scrollTop,
-                document.getElementById("idHocVien").getBoundingClientRect().top + document.documentElement.scrollTop,
-                document.getElementById("idLienHe").getBoundingClientRect().top + document.documentElement.scrollTop,
-                document.getElementById("idBaiViet").getBoundingClientRect().top + document.documentElement.scrollTop];
+let idOffSet = [document.getElementById("idTrangChu").getBoundingClientRect().top,
+                document.getElementById("idGioiThieu").getBoundingClientRect().top,
+                document.getElementById("idGiangVien").getBoundingClientRect().top,
+                document.getElementById("idNoiDung").getBoundingClientRect().top,
+                document.getElementById("idHocVien").getBoundingClientRect().top,
+                document.getElementById("idLienHe").getBoundingClientRect().top,
+                document.getElementById("idBaiViet").getBoundingClientRect().top];
 
 window.addEventListener("scroll",function()
   {
-    let windowViewpoint = window.pageYOffset + 100;
+    let windowViewpoint = window.pageYOffset;
 
     menuItemActive.forEach(function(element)
+      {
+        element.classList.remove("active");
+      });
+    burgerFilling.forEach(function(element)
       {
         element.classList.remove("active");
       });
@@ -72,29 +79,57 @@ window.addEventListener("scroll",function()
     if (idOffSet[1] > windowViewpoint && windowViewpoint >= idOffSet[0])
       {
         menuItemActive[0].classList.add("active");
+        burgerFilling[0].classList.add("active");
       }
     else if (idOffSet[2] > windowViewpoint && windowViewpoint >= idOffSet[1])
       {
         menuItemActive[1].classList.add("active");
+        burgerFilling[1].classList.add("active");
       }
     else if (idOffSet[3] > windowViewpoint && windowViewpoint >= idOffSet[2])
       {
         menuItemActive[2].classList.add("active");
+        burgerFilling[2].classList.add("active");
       }
     else if (idOffSet[4] > windowViewpoint && windowViewpoint >= idOffSet[3])
       {
         menuItemActive[3].classList.add("active");
+        burgerFilling[3].classList.add("active");
       }
     else if (idOffSet[5] > windowViewpoint && windowViewpoint >= idOffSet[4])
       {
         menuItemActive[4].classList.add("active");
+        burgerFilling[4].classList.add("active");
       }
     else if (idOffSet[6] > windowViewpoint && windowViewpoint >= idOffSet[5])
       {
         menuItemActive[5].classList.add("active");
+        burgerFilling[5].classList.add("active");
       }
     else
       {
         menuItemActive[6].classList.add("active");
+        burgerFilling[6].classList.add("active");
       }
   });
+
+
+  // ------------ ------------ ------------ ------------ ------------ ------------
+  // Hàm tạo hiệu ứng cho Humburger menu ------------ ------------
+  function hamburgerFunction() 
+    {
+      document.querySelector(".burger-menu").classList.toggle("burger-menu-toggle");
+      document.querySelector(".burger-menu-content").classList.toggle("burger-menu-toggle");
+      document.querySelector(".burger-menu-blackscreen").classList.toggle("burger-menu-toggle");
+    }
+
+
+  // ------------ ------------ ------------ ------------ ------------ ------------
+  // Hàm ẩn Humburger menu khi ấn vào từng menu con ------------ ------------
+  document.querySelectorAll(".burger-menu-content a").forEach(function(element)
+    {
+      element.addEventListener("click" ,function()
+        {
+          setTimeout(function(){ hamburgerFunction(); }, 600);
+        });
+    });
