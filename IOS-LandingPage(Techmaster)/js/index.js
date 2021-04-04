f73a2558e67c1670a57ceaf8d0936b5e2afe69c6
dungbubu <nguyenmanhdung1996@gmail.com>
2021-04-05 01:15:33 +0700

    5/4/21

--- IOS-LandingPage(Techmaster)/js/index.js
+++ IOS-LandingPage(Techmaster)/js/index.js
@@ -32,7 +32,6 @@ let menuItemActive = [...document.getElementsByClassName("itemMenu")];
 let burgerFilling = [...document.getElementsByClassName("burger-filling")];
 
 menuItemActive[0].classList.add("active");
-burgerFilling[0].classList.add("active");
 
 menuItemActive.forEach(function(element) 
   {
@@ -65,13 +64,9 @@ let idOffSet = [document.getElementById("idTrangChu").getBoundingClientRect().to
 
 window.addEventListener("scroll",function()
   {
-    let windowViewpoint = window.pageYOffset + 80;
+    let windowViewpoint = window.pageYOffset + 85;
 
     menuItemActive.forEach(function(element)
-      {
-        element.classList.remove("active");
-      });
-    burgerFilling.forEach(function(element)
       {
         element.classList.remove("active");
       });
@@ -79,37 +74,30 @@ window.addEventListener("scroll",function()
     if (idOffSet[1] > windowViewpoint && windowViewpoint >= idOffSet[0])
       {
         menuItemActive[0].classList.add("active");
-        burgerFilling[0].classList.add("active");
       }
     else if (idOffSet[2] > windowViewpoint && windowViewpoint >= idOffSet[1])
       {
         menuItemActive[1].classList.add("active");
-        burgerFilling[1].classList.add("active");
       }
     else if (idOffSet[3] > windowViewpoint && windowViewpoint >= idOffSet[2])
       {
         menuItemActive[2].classList.add("active");
-        burgerFilling[2].classList.add("active");
       }
     else if (idOffSet[4] > windowViewpoint && windowViewpoint >= idOffSet[3])
       {
         menuItemActive[3].classList.add("active");
-        burgerFilling[3].classList.add("active");
       }
     else if (idOffSet[5] > windowViewpoint && windowViewpoint >= idOffSet[4])
       {
         menuItemActive[4].classList.add("active");
-        burgerFilling[4].classList.add("active");
       }
     else if (idOffSet[6] > windowViewpoint && windowViewpoint >= idOffSet[5])
       {
         menuItemActive[5].classList.add("active");
-        burgerFilling[5].classList.add("active");
       }
     else
       {
         menuItemActive[6].classList.add("active");
-        burgerFilling[6].classList.add("active");
       }
   });
 
