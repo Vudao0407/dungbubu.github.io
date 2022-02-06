a08f19feb7e4a3c60e9e031b74d94c336d7cc3c4
dungbubu <nguyenmanhdung1996@gmail.com>
2022-02-06 15:35:32 +0700

    Update Responsive va phan chia lai file

---
+++ PortfolioVer2.2/js/hamburger.js
@@ -0,0 +1,5 @@
+// Hàm tạo hiệu ứng cho Humburger menu ------------ ------------
+function hamburgerFunction() {
+    document.querySelector(".burgerMenu").classList.toggle("toggle");
+    document.querySelector(".modal").classList.toggle("toggle");
+  }
