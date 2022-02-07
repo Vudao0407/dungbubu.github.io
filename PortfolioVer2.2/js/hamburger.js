30d6b40c6bfc870964ffcc80d93e763ed05b1a85
dungbubu <nguyenmanhdung1996@gmail.com>
2022-02-07 23:57:27 +0700

    Sua lai link font

--- PortfolioVer2.2/js/hamburger.js
+++
@@ -1,5 +0,0 @@
-// Hàm tạo hiệu ứng cho Humburger menu ------------ ------------
-function hamburgerFunction() {
-    document.querySelector(".burgerMenu").classList.toggle("toggle");
-    document.querySelector(".modal").classList.toggle("toggle");
-  }
