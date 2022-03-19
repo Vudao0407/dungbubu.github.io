13ea7f47e5bade3f55a8a09619e19888955a1f24
dungbubu <nguyenmanhdung1996@gmail.com>
2022-03-19 23:32:52 +0700

    Update font responsive

--- PortfolioVer2.2/js/index.js
+++ PortfolioVer2.2/js/index.js
@@ -4,27 +4,27 @@ console.log("%c😎 Hey, hi there!","font-size: 24px;",);
 console.log("%cWhatcha Doin'?","font-size: 12px;");
 console.log("%cWould you like a cup of coffee ☕?","font-size: 12px;");
 
-//Chuyển đổi chế độ Darkmode
-var root = document.querySelector(':root');
-
-switch (localStorage.getItem("data-theme")) {
-  case null:
-    $('.decorBg').css({"width":"0", "height":"0"});
-    root.setAttribute('data-theme', 'light');
-    localStorage.setItem("data-theme", "light");
-    break;
-  case "dark":
-    $('.decorBg').css({"width":"200vmax", "height":"200vmax"});
-    root.setAttribute('data-theme', 'dark');
-    $("#toggleDarkmode").html('lightmode');
-    break;
-  case "light":
-    $('.decorBg').css({"width":"0", "height":"0"});
-    root.setAttribute('data-theme', 'light');
-    $("#toggleDarkmode").html('darkmode');
-}
-
 window.onload = function () {
+  //Chuyển đổi chế độ Darkmode
+  var root = document.querySelector(':root');
+
+  switch (localStorage.getItem("data-theme")) {
+    case null:
+      $('.decorBg').css({"width":"0", "height":"0"});
+      root.setAttribute('data-theme', 'light');
+      localStorage.setItem("data-theme", "light");
+      break;
+    case "dark":
+      $('.decorBg').css({"width":"200vmax", "height":"200vmax"});
+      root.setAttribute('data-theme', 'dark');
+      $("#toggleDarkmode").html('lightmode');
+      break;
+    case "light":
+      $('.decorBg').css({"width":"0", "height":"0"});
+      root.setAttribute('data-theme', 'light');
+      $("#toggleDarkmode").html('darkmode');
+  }
+
   $(".toggleDarkmode").on('click', function () {
     let theme = localStorage.getItem("data-theme");
     
@@ -41,14 +41,16 @@ window.onload = function () {
       $('.decorBg').css({"width":"0", "height":"0"});
     }
   });
+
+
+  //Chuyển đổi chế độ Soundmode
+  $(".toggleSoundmode").on('click', function () {
+    var song = new Audio();
+    song.src = './audio/Merry go round of life - Morunas.mp3';
+    song.play();
+  });
 };
 
-//Chuyển đổi chế độ Soundmode
-$(".toggleSoundmode").on('click', function () {
-  var song = new Audio();
-  song.src = './audio/Merry go round of life - Morunas.mp3';
-  song.play();
-});
 
 // Humburger menu
 function hamburgerFunction() {
@@ -56,11 +58,12 @@ function hamburgerFunction() {
   $(".modal").toggleClass('toggle');
 }
 
+
 // Thay đổi con trỏ chuột
 $(window).mousemove(function(e) { 
   $('.cursor').css({left: e.pageX, top: e.pageY })
-})
 
-$('a, .flip-box')
-.on('mouseenter', function() {$('.cursor').addClass('focus')})
-.on('mouseleave', function() {$('.cursor').removeClass('focus')})
+  $('a, .flip-box')
+  .on('mouseenter', function() {$('.cursor').addClass('focus')})
+  .on('mouseleave', function() {$('.cursor').removeClass('focus')})
+})
